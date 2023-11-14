889296f15f972fd1b62018116602e0c9b9935fe7
sourav singh <104118305+hydradocs@users.noreply.github.com>
2023-11-14 12:39:30 +0530

    pages updated

--- business-website/src/App.jsx
+++ business-website/src/App.jsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './pages/Home'
+import Footer from './pages/Footer'
+import Product from './pages/Product'
+import Hero from './Hero'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
+    <>
+    {/* <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
+    </h1> */}
+    <Home />
+    <Hero/>
+    <Product/>
+    <Footer/>
+    </>
   )
 }
 
